2652d834f34ab6d5dd7e10803edce2606d8c4c23
securITymania <31162865+securITymania@users.noreply.github.com>
2019-07-05 15:52:31 +0530

    stable version

--- R3con_Bot.py
+++ R3con_Bot.py
@@ -5,9 +5,12 @@ import requests
 import time
 import re
 import sys
+import itertools
+import threading
 import optparse
 import dns.resolver
 import urllib.error
+from urllib.parse import urlparse
 from urllib.parse import urljoin
 from urllib import parse
 from urllib.request import urlopen, Request
@@ -17,13 +20,22 @@ from texttable import Texttable
 from blessings import Terminal
 
 parser = optparse.OptionParser()
-parser.add_option("-d","--domain", dest="hostname", help="Give the domain you want scan without 'https://'")
+parser.add_option("-d","--domain", dest="hostname", help="Give the target domain name ")
+parser.add_option("-s","--subdomain", dest="subdomain", help="These options is used to scan subdomains using 0 as skip and 1 as scan subdomains")
 parser.add_option("-t","--nmap_tcp", dest="nmap_tcp", help="Nmap TCP Scan type options from [1-4]             1.Fast scan[Top 100 port scan]                      2.Fast scan with service,os & version detection         3.Intense scan with service,os & version detection     4.All port scan with service,os & version detection")
 parser.add_option("-u","--nmap_udp", dest="nmap_udp", help="Nmap UDP Scan type options from [1-4]             1.Fast scan[Top 100 port scan]                      2.Fast scan with service,os & version detection         3.Intense scan with service,os & version detection        4.All port scan with service,os & version detection")
-parser.add_option("-s","--subdomain", dest="subdomain", help="To enable the subdomain Crawler by default this is disable mode")
+
 (options, arguments) = parser.parse_args()
 
-hostname = options.hostname
+host = options.hostname
+
+if host.startswith('http://'):
+    hostname = host.replace("http://","")
+elif host.startswith('https://'):
+    hostname = host.replace("https://","")
+else:
+    hostname = options.hostname
+
 nmap_tcp = options.nmap_tcp
 nmap_udp = options.nmap_udp
 subdomain = options.subdomain
@@ -42,25 +54,43 @@ print (t.bold_bright_yellow("""
                 		|__/  |__/ \______/  \_______/ \______/ |__/  |__/      |__/   |__/      \_______/|__/ |__/ |__/ \_______/ \_____/\___/  \______/ |__/      |__/  \__/
                                                                                                                                               
 """))
-print (t.bold_bright_cyan("[#] Created By   : Mr.Robot"))
+zero = "[#] Created By   :"
+first = "Mr.Robot"
+second = "IamNotRobot"
+print('{t.bold_bright_cyan}{} {t.bold_bright_red}{} , {t.bold_bright_red}{}'.format(zero,first, second, t=t))
 print (t.bold_bright_cyan("[#] Tool Support : R3con in Depth and Fully Automated"))
 print (t.bold_bright_cyan("[#] Version      : 1.2")) 
-
 print("\n")
-print (t.bold_bright_red("[#]Hold on Apache Server is starting.....!"))
+
+done = False
+#here is the animation
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\',]):
+        if done:
+            break
+        sys.stdout.write('\r[#] Hold on Apache Server is starting ......' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+t = threading.Thread(target=animate)
+t.start()
+time.sleep(3)
+done = True
+
 os.system("service apache2 start")
 os.system("mkdir /var/www/html/output")
-os.system("mkdir /var/www/html/output/wad")
 os.system("mkdir /var/www/html/output/nmap")
 os.system("clear")
 
 #Scan Information
 
 def scan_info(module,target_host):
-    modules = ('Whois','SSLScan','dns_enum','Nmap_TCP','Nmap_UDP','Robots.txt','Wad','searchsploit','secure_response_headers')
+    modules = ('Whois','Wad','searchsploit','secure_response_headers','SSLScan','dns_enum','Nmap_TCP','Nmap_UDP')
     f = open('/var/www/html/output/'+modules[module]+'.html', 'w')
     f.write("<h3>"+modules[module]+ " Scan Result: "+hostname+"</h3>")
     f.close()
+
+t = Terminal()
 
 #Printing target website
 print (t.bold_bright_green("""
@@ -118,7 +148,7 @@ try:
     print (t.bold_bright_cyan("[*] Usage: wad -u [Domain]"))
     cwd = os.getcwd()
     socket.setdefaulttimeout(60 * 60)
-    os.system("wad -u" + "http://" + hostname + " -f csv > /var/www/html/output/wad/wad.csv")
+    os.system("wad -u" + "http://" + hostname + " -f csv -o /var/www/html/output/wad/wad.csv 1> /dev/null")
     print("\n")
     time.sleep(8)
     
@@ -138,12 +168,12 @@ try:
         tt.reset()
         f.close()
     os.system("cat /var/www/html/output/wad/wad.txt")
-    scan_info(6,hostname)
+    scan_info(1,hostname)
     os.system("cat /var/www/html/output/wad/wad.txt|aha --black --word-wrap >> /var/www/html/output/Wad.html")  
     print("\n")
     change = cwd + "/input/webscreenshot.py"
-    os.system("python " + change + " 127.0.0.1/output/Wad.html" + " -o " + cwd + "/output/screenshot/"+hostname)
-    	
+    os.system("python " + change + " 127.0.0.1/output/Wad.html" + " -o " + cwd + "/output/screenshot/" + hostname)
+        
 except Exception as e:
     print(e)
 
@@ -162,8 +192,9 @@ for key,val in versions.items():
         os.system(cmd)
     else:
         pass
-scan_info(7,hostname)
+scan_info(2,hostname)
 os.system("cat /var/www/html/output/searchsploit.txt")
+print("\n")
 os.system("cat /var/www/html/output/searchsploit.txt|aha  --word-wrap >> /var/www/html/output/searchsploit.html")
 change = cwd + "/input/webscreenshot.py"
 os.system("python " + change + " 127.0.0.1/output/searchsploit.html" + " -o " + cwd + "/output/screenshot/"+hostname)
@@ -217,7 +248,7 @@ try:
     for key,val in headers.items():
         if key != 'Date' and key != 'Link' and key != 'X-UA-Compatible' and key != 'Content-language' and key != 'Expires' and key != 'Vary' and key != 'Cache-Tags' and key != 'Last-Modified' and key != 'ETag' and key != 'Transfer-Encoding' and key != 'Connection' and key != 'Accept-Ranges' and key!= 'Content-Encoding':
             domainheaders = '{t.green}{:35} :{t.white}{}'.format(key, val, t=t)
-            print('{t.green}{:35} : {t.white}{}'.format(key, val, t=t))
+            print('{t.bold_bright_green}{:35} :  {t.bold_bright_white}{}'.format(key, val, t=t))
             host_headers.append(key)
             with open('/var/www/html/output/response.txt', 'a') as f:
                 print(domainheaders, file=f)
@@ -267,11 +298,11 @@ try:
     for i in secure_response_headers:
         if i not in host_headers:
             missings = '{t.red}{}'.format(i, t=t)
-            print(t.bold_bright_red("[-] "+i))
+            print(t.bold_bright_red("[-] " + i))
             with open('/var/www/html/output/response.txt', 'a') as f:
                 print(missings, file=f)
-    scan_info(8,hostname)
-    os.system("cat /var/www/html/output/response.txt|aha --word-wrap --black >> /var/www/html/output/secure_response_headers.html")
+    scan_info(3,hostname)
+    os.system("cat /var/www/html/output/response.txt|aha --word-wrap  >> /var/www/html/output/secure_response_headers.html")
     print("\n")
     change = cwd + "/input/webscreenshot.py"
     os.system("python " + change + " 127.0.0.1/output/secure_response_headers.html" + " -o " + cwd + "/output/screenshot/"+hostname)
@@ -291,9 +322,9 @@ try:
     print("\n")
     os.system("sslscan --no-fallback " + hostname + " > " + "/var/www/html/output/sslscan.txt")
     os.system("cat /var/www/html/output/sslscan.txt")
-    scan_info(1,hostname)
+    scan_info(4,hostname)
     print("\n")
-    os.system("cat /var/www/html/output/sslscan.txt|aha --word-wrap --black >> /var/www/html/output/SSLScan.html")
+    os.system("cat /var/www/html/output/sslscan.txt|aha --word-wrap  >> /var/www/html/output/SSLScan.html")
     print("\n")
     change = cwd + "/input/webscreenshot.py"
     os.system("python " + change + " 127.0.0.1/output/SSLScan.html" + " -o " + cwd + "/output/screenshot/"+hostname)
@@ -312,7 +343,7 @@ try:
     print("\n")
     os.system("dnsenum --enum " + hostname + " > " + "/var/www/html/output/dns_enum.txt")
     os.system("cat /var/www/html/output/dns_enum.txt")
-    scan_info(2,hostname)
+    scan_info(5,hostname)
     os.system("cat /var/www/html/output/dns_enum.txt|aha --word-wrap >> /var/www/html/output/dns_enum.html")
     print("\n")
     change = cwd + "/input/webscreenshot.py"
@@ -337,7 +368,7 @@ def nmap_scan(nmap_tcp):
         os.system(nmap_commands[nmap_tcp] + IP + " > " + "/var/www/html/output/nmap_tcp.txt" )
         os.system("cat " + "/var/www/html/output/nmap_tcp.txt " + "| egrep -i 'state|open|filtered' " + " > " + "/var/www/html/output/nmap_tcp1.txt")
         os.system("cat " + "/var/www/html/output/nmap_tcp1.txt")
-        scan_info(3,hostname)
+        scan_info(6,hostname)
         os.system("cat /var/www/html/output/nmap_tcp.txt|aha --word-wrap >> /var/www/html/output/Nmap_TCP.html")
         print("\n")
         change = cwd + "/input/webscreenshot.py"
@@ -363,8 +394,8 @@ def nmap_udp_scan(nmap_udp):
         os.system(nmap_udp_commands[nmap_udp] + IP + " > " + "/var/www/html/output/nmap_udp.txt" )
         os.system("cat " + "/var/www/html/output/nmap_udp.txt " + "| egrep -i 'state|open|filtered' " + " > " + "/var/www/html/output/nmap_udp1.txt")
         os.system("cat " + "/var/www/html/output/nmap_udp1.txt")
-        scan_info(4,hostname)
-        os.system("cat /var/www/html/output/nmap_udp.txt|aha --word-wrap --black >> /var/www/html/output/Nmap_UDP.html")
+        scan_info(7,hostname)
+        os.system("cat /var/www/html/output/nmap_udp.txt|aha --word-wrap  >> /var/www/html/output/Nmap_UDP.html")
         print("\n")
         change = cwd + "/input/webscreenshot.py"
         os.system("python " + change + " 127.0.0.1/output/Nmap_UDP.html" + " -o " + cwd + "/output/screenshot/" + hostname)
@@ -423,14 +454,11 @@ try:
         with open("/var/www/html/output/robot1.txt", "wb") as f:
             f.write(read)
             f.close()
-            scan_info(5,hostname)
-            os.system("cat /var/www/html/output/robot1.txt " + " | grep -i 'allow' " + " | awk '{print $2}' " + " > " + "/var/www/html/output/Robots.html")
-            with open("/var/www/html/output/Robots.html", "r") as f:
+            os.system("cat /var/www/html/output/robot1.txt " + " | grep -i 'allow' " + " | awk '{print $2}' " + " > " + "/var/www/html/output/robot2.txt")
+            with open("/var/www/html/output/robot2.txt", "r") as f:
                 url	= f.read().split()
                 f.close()
-            change = cwd + "/input/webscreenshot.py"
-            os.system("python " + change + " 127.0.0.1/output/Robots.html" + " -o " + cwd + "/output/screenshot/"+hostname)
-            print("\n")
+           
             for i in url:
                 q.put(i.strip())
             q.join()
@@ -503,97 +531,97 @@ print (t.bold_bright_green("""
 #                                Sub-Domain                                        #
 ====================================================================================
 """))
-time.sleep(1)
-concurrent = 10
+if subdomain == '1':
+    time.sleep(1)
+    concurrent = 10
+
+    def doWork():
+    	while True:
+    		try:
+    			netloc = q.get()
+    			url = "http://" + netloc
+    			status, url = getStatus(url)
+    			doSomethingWithResult(status, url)
+    			q.task_done()
+    		except requests.exceptions.ConnectionError as e:
+    			q.task_done()
+    			pass
 
 
-def doWork():
-	while True:
-		try:
-			netloc = q.get()
-			url = "http://" + netloc
-			status, url = getStatus(url)
-			doSomethingWithResult(status, url)
-			q.task_done()
-		except requests.exceptions.ConnectionError as e:
-			q.task_done()
-			pass
+    def getStatus(ourl):
+       try:
+           headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0" }
+           res = requests.get(ourl, headers= headers)
+           return res.status_code, ourl
+       except:
+           return "No Response Code", ourl
 
+    def cname(c_url):
+       try:
+          result = dns.resolver.query(c_url, 'CNAME')
+          for cnameval in result:
+             return ' CNAME:', cnameval.target
+       except:
+           return "error2", c_url
 
-def getStatus(ourl):
-   try:
-       headers = { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0" }
-       res = requests.get(ourl, headers= headers)
-       return res.status_code, ourl
-   except:
-       return "No Response Code", ourl
+    def nmap_fun(url1):
+    	
+    	IP = socket.gethostbyname(url1)
 
-def cname(c_url):
-   try:
-      result = dns.resolver.query(c_url, 'CNAME')
-      for cnameval in result:
-         return ' CNAME:', cnameval.target
-   except:
-       return "error2", c_url
+    	os.system("nmap -Pn -F -T4 " + IP + " > " + "/var/www/html/output/nmap/nmap_tcp.txt" )
+    	os.system("cat /var/www/html/output/nmap/nmap_tcp.txt | egrep -i 'state|open' > /var/www/html/output/nmap/nmap_tcp1.txt")
+    	os.system("""cat /var/www/html/output/nmap/nmap_tcp1.txt | sed 's/[^0-9]*//g' > /var/www/html/output/nmap/nmap_tcp2.txt""")
+    	os.system("""cat /var/www/html/output/nmap/nmap_tcp2.txt | sed '/^$/d' > /var/www/html/output/nmap/nmap_tcp3.txt""")
+    	os.system("""cat /var/www/html/output/nmap/nmap_tcp3.txt | sed '$!s/$/,/' > /var/www/html/output/nmap/nmap_tcp4.txt""")
+    	os.system("""cat /var/www/html/output/nmap/nmap_tcp4.txt | tr -d '\n'  > /var/www/html/output/nmap/nmap_tcp5.txt""")
 
-def nmap_fun(url1):
-	
-	IP = socket.gethostbyname(url1)
+    	with open("/var/www/html/output/nmap/nmap_tcp5.txt", "r")as f:
+    		order = f.read()
+    		f.close()
+    		return "PORT: ", order
 
-	os.system("nmap -Pn -F -T4 " + IP + " > " + "/var/www/html/output/nmap/nmap_tcp.txt" )
-	os.system("cat /var/www/html/output/nmap/nmap_tcp.txt | egrep -i 'state|open' > /var/www/html/output/nmap/nmap_tcp1.txt")
-	os.system("""cat /var/www/html/output/nmap/nmap_tcp1.txt | sed 's/[^0-9]*//g' > /var/www/html/output/nmap/nmap_tcp2.txt""")
-	os.system("""cat /var/www/html/output/nmap/nmap_tcp2.txt | sed '/^$/d' > /var/www/html/output/nmap/nmap_tcp3.txt""")
-	os.system("""cat /var/www/html/output/nmap/nmap_tcp3.txt | sed '$!s/$/,/' > /var/www/html/output/nmap/nmap_tcp4.txt""")
-	os.system("""cat /var/www/html/output/nmap/nmap_tcp4.txt | tr -d '\n'  > /var/www/html/output/nmap/nmap_tcp5.txt""")
+    def doSomethingWithResult(status, url):
+    	url1 = url.replace("http://","")
+    	if status == 200:
+    		order = nmap_fun(url1)
+    		time.sleep(1)
+    		print(t.bold_bright_yellow("[+]Status_Code [" + str(status) + "] "  + "  : " + url + " : "),str(order))
+    	elif status == 404:
+    		c_name = cname(url1)
+    		time.sleep(1)
+    		print(t.bold_bright_red("[-]Status_Code [" + str(status) + "] "  + "  : " + url + " : "), str(c_name))
+    	elif(status != 200 and status != 404):
+    		pass
+        
+    q = Queue(concurrent * 2)
 
-	with open("/var/www/html/output/nmap/nmap_tcp5.txt", "r")as f:
-		order = f.read()
-		f.close()
-		return "PORT: ", order
+    for i in range(concurrent):
+    	th1 = Thread(target=doWork)
+    	th1.daemon = True
+    	th1.start()
 
-def doSomethingWithResult(status, url):
-	url1 = url.replace("http://","")
-	if status == 200:
-		order = nmap_fun(url1)
-		time.sleep(1)
-		print(t.bold_bright_yellow("[+]Status_Code [" + str(status) + "] "  + "  : " + url + " : "),str(order))
-	elif status == 404:
-		c_name = cname(url1)
-		time.sleep(1)
-		print(t.bold_bright_red("[-]Status_Code [" + str(status) + "] "  + "  : " + url + " : "), str(c_name))
-	elif(status != 200 and status != 404):
-		pass
-    
-q = Queue(concurrent * 2)
+    url = 'https://www.virustotal.com/vtapi/v2/domain/report'
+    params = {'apikey': '83f01852d8f7dbd65b2a2a31e43f2b79a4f91ac7f14f85ab9e1fb9392c51c3ea', 'domain': hostname }
+    response = requests.get(url, params=params)
+    data = response.json()
+    try:
+      sub1 = data["subdomains"]
+    except:
+      sub1 = []
+    sub2 = data["domain_siblings"]
+    subdomains = sub1 + sub2
 
-for i in range(concurrent):
-	th1 = Thread(target=doWork)
-	th1.daemon = True
-	th1.start()
-def subdomain_crawler(hostname):
-	url = 'https://www.virustotal.com/vtapi/v2/domain/report'
-	params = {'apikey': '83f01852d8f7dbd65b2a2a31e43f2b79a4f91ac7f14f85ab9e1fb9392c51c3ea', 'domain': hostname }
-	response = requests.get(url, params=params)
-	data = response.json()
-	try:
-		sub1 = data["subdomains"]
-	except:
-		sub1 = []
-	sub2 = data["domain_siblings"]
-	subdomains = sub1 + sub2
-	t = Terminal()
-	try:
-		for url in subdomains:
-			q.put(url.strip())
-		q.join()
-	except:
-		pass
+    t = Terminal()
 
-if  subdomain == None:
-	print(t.bold_bright_yellow("*******subdomain crawler is disabled*********"))
-else:
-	subdomain_crawler(hostname)
+    try:
+    	for url in subdomains:
+    		q.put(url.strip())
+    	q.join()
+    except:
+    	pass
+
+elif subdomain == '0':
+    print (t.bold_bright_red("Sub-domains scan has been skiped by the user"))
 
 
 #Critical-File Finding
@@ -604,8 +632,10 @@ print (t.bold_bright_green("""
 """))
 time.sleep(1)
 concurrent = 10
-print (t.bold_bright_cyan("[*] Usage: https://hostname/[Wordlist]"))
+url = "http://" + hostname
+print (t.bold_bright_yellow("Target: "),url)
 print("\n")
+print(t.bold_bright_cyan(""))
 def critical_file_found():
 	while True:
 		try:
@@ -623,13 +653,10 @@ def critical_file_found():
 def code(status,url,redirect):
 	try:
 		if status == 200:
-			time.sleep(1)
 			print(t.bold_bright_yellow("[+]Status_Code [" + str(status) + "] "  + "  : " + url + " --> "),redirect )
 		elif status == 401:
-			time.sleep(1)
 			print(t.bold_bright_red("[-]Status_Code [" + str(status) + "] "  + "  : " + url + " --> "),redirect)
 		elif status == 403:
-			time.sleep(1)
 			print(t.bold_bright_red("[-]Status_Code [" + str(status) + "] "  + "  : " + url + " --> "), redirect)
 		else:
 			pass
@@ -658,32 +685,3 @@ try:
 			
 except Exception as e:
 	print(e)
-
-#Web Crawler
-print (t.bold_bright_green("""
-====================================================================================
-#                                     Web Crawler                                  #
-====================================================================================
-"""))
-target_url = "https://" + hostname
-target_links = []
-
-def extract_links_from(url):
-	response = requests.get(url)
-	content = response.content
-	return re.findall('(?:href=")(.*?)"',str(content))
-
-def crawl(url):
-	href_links = extract_links_from(url)
-	for link in href_links:
-		link = urljoin(url, link)
-
-		if "#" in link:
-			link = link.split("#")[0]
-
-		if target_url in link and link not in target_links:
-			target_links.append(link)
-			print(t.bold_bright_green("[+]URL Found: "),link)
-			crawl(link)
-
-crawl(target_url)
